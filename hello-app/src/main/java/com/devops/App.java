e623f946f3857f9f833ff83fb0be3eb0dac0c94c
sneha shingade <snehashingade716@gmail.com>
2026-07-16 02:04:48 +0530

    Added Maven Java application

---
+++ hello-app/src/main/java/com/devops/App.java
@@ -0,0 +1,14 @@
+package com.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Devops CI/CD Pipeline Project" );
+        System.out.println( "Developed by Sneha Shingade" );
+    }
+}
